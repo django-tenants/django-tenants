fef4c2c1fc0f39dc20aa9ae709e418b10feb0609
Konrad Szymański <ko.szymanski@gmail.com>
2015-07-06 09:15:18 +0200

    Fix for AppRegistryNotReady when PG_EXTRA_SEARCH_PATHS setting is used
    Django 1.8 proper initialization and check should be done at AppConfig ready method.
    This way models are not loaded to early what is fixing this bug.

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ try:
 except ImportError:
     from distutils.core import setup
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 setup(
     name='django-tenants',
-    version='1.0.1',
+    version='1.0.2',
     author='Thomas Turner',
     author_email='tom@twt.me.uk',
     packages=[
