d4c6a6c8e90c249694838381ce76a239c004041c
Przemek Kaminski <cgenie@gmail.com>
2015-10-15 13:20:13 +0200

    Parallel execution: fix migration execution getting from os.environ

--- django_tenants/migration_executors/__init__.py
+++ django_tenants/migration_executors/__init__.py
@@ -1,10 +1,12 @@
+import os
+
 from .base import MigrationExecutor
 from .multiproc import MultiprocessingExecutor
 from .standard import StandardExecutor
 
 
 def get_executor(codename=None):
-    codename = codename or StandardExecutor.codename
+    codename = codename or os.environ.get('EXECUTOR', StandardExecutor.codename)
 
     for klass in MigrationExecutor.__subclasses__():
         if klass.codename == codename:
