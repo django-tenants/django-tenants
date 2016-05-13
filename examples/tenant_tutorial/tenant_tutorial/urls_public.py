23d81d601bb77c7efda1e4782a19c2e81f0378b9
Thomas Turner <tom@twt.me.uk>
2016-05-13 14:44:04 +0100

    Another small change to the admin

--- examples/tenant_tutorial/tenant_tutorial/urls_public.py
+++ examples/tenant_tutorial/tenant_tutorial/urls_public.py
@@ -3,6 +3,8 @@ from tenant_tutorial.views import HomeView
 from django.contrib import admin
 urlpatterns = [
     url(r'^$', HomeView.as_view()),
-
+    url(
+        r'^admin/',
+        include(admin.site.urls)),
 
     ]
