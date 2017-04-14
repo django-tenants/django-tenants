9052b4ef29ec8c2a194a7f7b5ebeee84adc6b964
Thomas Turner <tom@twt.me.uk>
2017-04-14 20:52:43 +0100

    Work done on django 1.11

--- django_tenants/postgresql_backend/base.py
+++ django_tenants/postgresql_backend/base.py
@@ -108,12 +108,16 @@ class DatabaseWrapper(original_backend.DatabaseWrapper):
                       category=DeprecationWarning)
         return self.tenant
 
-    def _cursor(self):
+    def _cursor(self, name=None):
         """
         Here it happens. We hope every Django db operation using PostgreSQL
         must go through this to get the cursor handle. We change the path.
         """
-        cursor = super(DatabaseWrapper, self)._cursor()
+        if name:
+            # Only supported and required by Django 1.11 (server-side cursor)
+            cursor = super(DatabaseWrapper, self)._cursor(name=name)
+        else:
+            cursor = super(DatabaseWrapper, self)._cursor()
 
         # optionally limit the number of executions - under load, the execution
         # of `set search_path` can be quite time consuming
@@ -139,16 +143,26 @@ class DatabaseWrapper(original_backend.DatabaseWrapper):
                 search_paths = [self.schema_name]
 
             search_paths.extend(EXTRA_SEARCH_PATHS)
+
+            if name:
+                # Named cursor can only be used once
+                cursor_for_search_path = self.connection.cursor()
+            else:
+                # Reuse
+                cursor_for_search_path = cursor
+
             # In the event that an error already happened in this transaction and we are going
             # to rollback we should just ignore database error when setting the search_path
             # if the next instruction is not a rollback it will just fail also, so
             # we do not have to worry that it's not the good one
             try:
-                cursor.execute('SET search_path = {0}'.format(','.join(search_paths)))
+                cursor_for_search_path.execute('SET search_path = {0}'.format(','.join(search_paths)))
             except (django.db.utils.DatabaseError, psycopg2.InternalError):
                 self.search_path_set = False
             else:
                 self.search_path_set = True
+            if name:
+                cursor_for_search_path.close()
         return cursor
 
 
--- django_tenants/test/cases.py
+++ django_tenants/test/cases.py
@@ -1,11 +1,25 @@
 from django.core.management import call_command
+from django.conf import settings
 from django.db import connection
-from django.test import TransactionTestCase
+from django.test import TestCase
 
 from django_tenants.utils import get_tenant_model, get_tenant_domain_model, get_public_schema_name
 
+ALLOWED_TEST_DOMAIN = '.test.com'
 
-class TenantTestCase(TransactionTestCase):
+
+class TenantTestCase(TestCase):
+
+    def add_allowed_test_domain(cls):
+        # ALLOWED_HOSTS is a special setting of Django setup_test_environment so we can't modify it with helpers
+        if ALLOWED_TEST_DOMAIN not in settings.ALLOWED_HOSTS:
+            settings.ALLOWED_HOSTS += [ALLOWED_TEST_DOMAIN]
+
+    @classmethod
+    def remove_allowed_test_domain(cls):
+        if ALLOWED_TEST_DOMAIN in settings.ALLOWED_HOSTS:
+            settings.ALLOWED_HOSTS.remove(ALLOWED_TEST_DOMAIN)
+
     def setup_tenant(self, tenant):
         """
         Add any additional setting to the tenant before it get saved. This is required if you have
@@ -24,24 +38,28 @@ class TenantTestCase(TransactionTestCase):
         """
         pass
 
-    def setUp(self):
-        self.sync_shared()
-        self.tenant = get_tenant_model()(schema_name=self.get_test_schema_name())
-        self.setup_tenant(self.tenant)
-        self.tenant.save(verbosity=0)  # todo: is there any way to get the verbosity from the test command here?
+    @classmethod
+    def setUpClass(cls):
+        cls.sync_shared()
+        cls.add_allowed_test_domain()
+        cls.tenant = get_tenant_model()(schema_name=cls.get_test_schema_name())
+        cls.setup_tenant(cls.tenant)
+        cls.tenant.save(verbosity=0)  # todo: is there any way to get the verbosity from the test command here?
 
         # Set up domain
-        tenant_domain = self.get_test_tenant_domain()
-        self.domain = get_tenant_domain_model()(tenant=self.tenant, domain=tenant_domain)
-        self.setup_domain(self.domain)
-        self.domain.save()
+        tenant_domain = cls.get_test_tenant_domain()
+        cls.domain = get_tenant_domain_model()(tenant=cls.tenant, domain=tenant_domain)
+        cls.setup_domain(cls.domain)
+        cls.domain.save()
 
-        connection.set_tenant(self.tenant)
+        connection.set_tenant(cls.tenant)
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(cls):
         connection.set_schema_to_public()
-        self.domain.delete()
-        self.tenant.delete(force_drop=True)
+        cls.remove_allowed_test_domain()
+        cursor = connection.cursor()
+        cursor.execute('DROP SCHEMA IF EXISTS test CASCADE')
 
     @classmethod
     def sync_shared(cls):
@@ -64,6 +82,7 @@ class FastTenantTestCase(TenantTestCase):
     @classmethod
     def setUpClass(cls):
         cls.sync_shared()
+        cls.add_allowed_test_domain()
         tenant_model = get_tenant_model()
 
         test_schema_name = cls.get_test_schema_name()
@@ -82,7 +101,7 @@ class FastTenantTestCase(TenantTestCase):
     @classmethod
     def tearDownClass(cls):
         connection.set_schema_to_public()
-
+        cls.remove_allowed_test_domain()
 
     def tearDown(self):
         """
