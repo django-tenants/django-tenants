9bd3940412878ce9ea1021bc3b9eda709ad8c7e3
viatrak <justin@milker.io>
2017-09-07 12:43:40 -0500

    Content type cache should be cleared not just at the beginning of a request, but *anytime* the schema is changed (which happens in middle of requests). set_tenant() will also now result in a set_schema() call so it will claer_cache() at the beginning of a request, and on subsequent set_schema calls

--- django_tenants/postgresql_backend/base.py
+++ django_tenants/postgresql_backend/base.py
@@ -3,9 +3,10 @@ import warnings
 from django.conf import settings
 from importlib import import_module
 
-from django.core.exceptions import ImproperlyConfigured, ValidationError
-from django_tenants.utils import get_public_schema_name, get_limit_set_calls
 from django_tenants.postgresql_backend.introspection import DatabaseSchemaIntrospection
+from django_tenants.utils import get_public_schema_name, get_limit_set_calls
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ImproperlyConfigured, ValidationError
 import django.db.utils
 import psycopg2
 
@@ -84,6 +85,14 @@ class DatabaseWrapper(original_backend.DatabaseWrapper):
         self.include_public_schema = include_public
         self.set_settings_schema(schema_name)
         self.search_path_set = False
+        # Content type can no longer be cached as public and tenant schemas
+        # have different models. If someone wants to change this, the cache
+        # needs to be separated between public and shared schemas. If this
+        # cache isn't cleared, this can cause permission problems. For example,
+        # on public, a particular model has id 14, but on the tenants it has
+        # the id 15. if 14 is cached instead of 15, the permissions for the
+        # wrong model will be fetched.
+        ContentType.objects.clear_cache()
 
     def set_schema_to_public(self):
         """
