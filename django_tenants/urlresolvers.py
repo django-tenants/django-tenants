ee8f9fd76ab9552533b6e0ede9a83c72a15b75f6
Erik Rivera <erik.river@gmail.com>
2017-12-06 00:47:28 -0600

    Fixed urlresolvers to support django 2.0

--- django_tenants/urlresolvers.py
+++ django_tenants/urlresolvers.py
@@ -1,6 +1,10 @@
-from django.core.urlresolvers import reverse as reverse_default
 from django.utils.functional import lazy
 from django_tenants.utils import clean_tenant_url
+
+try:  # Django >= 2.0
+    from django.urls import reverse as reverse_default
+except ImportError:
+    from django.core.urlresolvers import reverse as reverse_default
 
 
 def reverse(viewname, urlconf=None, args=None, kwargs=None, current_app=None):
