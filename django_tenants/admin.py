51b29aa16f29a0f991d49d396f6e2428ea528a65
Domenico DiNicola <dom.dinicola@gmail.com>
2019-01-15 16:38:21 -0500

    documentation

--- django_tenants/admin.py
+++ django_tenants/admin.py
@@ -1,2 +1,6 @@
 class TenantAdminMixin(object):
+    """
+    Mixin for Tenant model:
+    It disables save and delete buttons when not in current or public tenant (preventing Exceptions).
+    """
     change_form_template = 'admin/django_tenants/tenant/change_form.html'
