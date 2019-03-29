a031ae6106deb95ade3887aeecf21d9457cd5323
Mr. Senko <atodorov@mrsenko.com>
2019-03-30 01:28:57 +0200

    Remove obsolete test_default() and test_format_string() tests
    from TenantFileSystemStorageTestCase. I consider these to be
    obsolete, even wrong, because they do not exercise
    TenantFileSystemStorage in the context of different tenants, not
    do they try to simulate file upload/download.

--- django_tenants/tests/test_filesystem_storage.py
+++ django_tenants/tests/test_filesystem_storage.py
@@ -16,54 +16,6 @@ class TenantFileSystemStorageTestCase(TenantTestCase):
         settings.MEDIA_ROOT = "apps_dir/media"
         settings.MEDIA_URL = "/media/"
 
-    def test_default(self):
-        storage = TenantFileSystemStorage()
-
-        # location
-        path_suffix = "{}/{}".format(settings.MEDIA_ROOT, self.tenant.schema_name)
-        self.assertEqual(storage.location[-len(path_suffix):], path_suffix)
-
-        # path
-        path_suffix = "{}/{}/foo.txt".format(
-            settings.MEDIA_ROOT, self.tenant.schema_name
-        )
-        self.assertEqual(storage.path("foo.txt")[-len(path_suffix):], path_suffix)
-
-        # base_url
-        self.assertEqual(storage.base_url, "/media/{}/".format(self.tenant.schema_name))
-
-        # url
-        self.assertEqual(
-            storage.url("foo.txt"), "/media/{}/foo.txt".format(self.tenant.schema_name)
-        )
-
-    def test_format_string(self):
-        settings.MULTITENANT_RELATIVE_MEDIA_ROOT = "%s/other_dir"
-        storage = TenantFileSystemStorage()
-
-        # location
-        path_suffix = "{}/{}/other_dir".format(
-            settings.MEDIA_ROOT, self.tenant.schema_name
-        )
-        self.assertEqual(storage.location[-len(path_suffix):], path_suffix)
-
-        # path
-        path_suffix = "{}/{}/other_dir/foo.txt".format(
-            settings.MEDIA_ROOT, self.tenant.schema_name
-        )
-        self.assertEqual(storage.path("foo.txt")[-len(path_suffix):], path_suffix)
-
-        # base_url
-        self.assertEqual(
-            storage.base_url, "/media/{}/other_dir/".format(self.tenant.schema_name)
-        )
-
-        # url
-        self.assertEqual(
-            storage.url("foo.txt"),
-            "/media/{}/other_dir/foo.txt".format(self.tenant.schema_name),
-        )
-
     def test_deprecated_module_raises_warning(self):
         with warnings.catch_warnings(record=True) as warns:
             deprecation_warning = "TenantFileSystemStorage has been moved from django_tenants.files.storages " \
