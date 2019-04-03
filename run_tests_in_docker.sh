1fe685d9ae2ef186124ba77f9dde00a20bff6e7a
Alexander Nestorov <alexandernst@gmail.com>
2019-04-03 17:33:09 +0200

    Stop killing the container after running the tests

--- run_tests_in_docker.sh
+++ run_tests_in_docker.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 python3 setup.py develop
-./run_tests.sh
+
+echo "docker attach the tty in order to manage this"
+while true; do
+    echo "Run tests? [yn]" # can't use "read -p", no real tty
+    read yn
+    case $yn in
+        [Yy] ) ./run_tests.sh;continue;;
+        * ) exit;;
+    esac
+done
