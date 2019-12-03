44b0bf78a5ccabcb28c4fa4ef0465aadc3125d1c
tomturner <tom@twt.me.uk>
2019-12-03 21:45:56 +0000

    Updated vagrant for Django 3

--- Vagrantfile
+++ Vagrantfile
@@ -2,7 +2,12 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
-  config.vm.box = "bento/ubuntu-16.04"
+  config.vm.box = "bento/ubuntu-18.04"
+
+  config.vm.box = "django_tenants_bionic_beaver"
+
+  config.vm.box_url = "https://app.vagrantup.com/bento/boxes/ubuntu-18.04/versions/201808.24.0/providers/virtualbox.box"
+
   config.vm.network :forwarded_port, guest: 8088, host: 8088
   config.vm.network :forwarded_port, guest: 22, host: 2222, id: "ssh", disabled: true
   config.vm.network :forwarded_port, guest: 22, host: 2020, auto_correct: true
--- fabfile.py
+++ fabfile.py
@@ -56,38 +56,33 @@ def update_requirements():
                                    'python3-pip',
                                    'python3-psycopg2',
                                    'git',
-                                   'libboost-python1.58.0',
+                                   'libboost-python1.*.0',
                                    'pkg-config',
-                                   'postgresql-server-dev-9.5',
+                                   'postgresql-server-dev-10',
                                    'postgresql-contrib',
                                    ])
 
-    sudo("pip3 install django==2.1.5")
+    sudo("pip3 install django==3.0")
+
 
 def install_database(name, owner, template='template0', encoding='UTF8', locale='en_US.UTF-8'):
-    """
-    Require a PostgreSQL database.
-
-    ::
-
-        from fabtools import require
-
-        require.postgres.database('myapp', owner='dbuser')
-
-    """
-
-
     create_database(name, owner, template=template, encoding=encoding,
-                        locale=locale)
+                    locale=locale)
 
 @task
 def create_pg_database():
     # fabtools.require.postgres.server()
-    fabtools.require.postgres.user(env.psql_user, env.psql_password, createdb=True)
-    # fabtools.require.postgres.database(env.psql_db, env.psql_user)
+    sudo('service postgresql start', shell=False)
 
-    sudo("sed -i 's/all                                     peer/all"
-         "                                     md5/g' /etc/postgresql/9.5/main/pg_hba.conf")
+    sudo('psql -c "DROP DATABASE IF EXISTS %s;"' % env.psql_db, user='postgres')
+    sudo('psql -c "DROP USER IF EXISTS jmscloud;"', user='postgres')
+    sudo('psql -c "CREATE USER %s WITH PASSWORD \'%s\';"' % (env.psql_user, env.psql_password), user='postgres')
+    sudo('psql -c "ALTER USER %s WITH SUPERUSER;"' % env.psql_user, user='postgres')
+
+    install_database(env.psql_db, env.psql_user)
+
+    sudo("sed -i 's/all                                     peer/"
+         "all                                     md5/g' /etc/postgresql/10/main/pg_hba.conf")
     sudo('service postgresql restart')
     install_database(env.psql_db, env.psql_user)
 
