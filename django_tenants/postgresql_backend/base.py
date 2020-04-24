ab105f549a6fa686d0c9234423cc1af26368a243
Tom Turner <tom@twt.me.uk>
2020-04-24 15:43:19 +0100

    Fixed #386 allowed access to is_valid_schema_name

--- django_tenants/postgresql_backend/base.py
+++ django_tenants/postgresql_backend/base.py
@@ -34,12 +34,12 @@ def _check_identifier(identifier):
         raise ValidationError("Invalid string used for the identifier.")
 
 
-def _is_valid_schema_name(name):
+def is_valid_schema_name(name):
     return _is_valid_identifier(name) and not SQL_SCHEMA_NAME_RESERVED_RE.match(name)
 
 
 def _check_schema_name(name):
-    if not _is_valid_schema_name(name):
+    if not is_valid_schema_name(name):
         raise ValidationError("Invalid string used for the schema name.")
 
 
