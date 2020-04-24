3137ebcd8a398a6a8c49ae44c6cee51cb582fffb
Tom Turner <tom@twt.me.uk>
2020-04-24 20:01:48 +0100

    Another fix for the rename test

--- django_tenants/tests/test_tenants.py
+++ django_tenants/tests/test_tenants.py
@@ -564,7 +564,7 @@ class TenantRenameSchemaTest(BaseTestCase):
         self.assertTrue(schema_exists(tenant.schema_name))
         domain = get_tenant_domain_model()(tenant=tenant, domain='something.test.com')
         domain.save()
-        schema_rename(tenant=Client.objects.filter(pk=tenant.pk), new_schema_name='new_name')
+        schema_rename(tenant=Client.objects.filter(pk=tenant.pk).first(), new_schema_name='new_name')
         self.assertTrue(schema_exists('test'))
         self.assertFalse(schema_exists('new_name'))
 
