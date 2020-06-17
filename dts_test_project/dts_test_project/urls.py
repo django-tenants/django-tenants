8c0c2eb8dcac79e02e23838ef98cc5ae0e861bb2
Lorenzo Peña <lorinkoz@gmail.com>
2020-06-17 15:53:10 -0400

    Added basic tests for URL reversing with subfolders

--- dts_test_project/dts_test_project/urls.py
+++ dts_test_project/dts_test_project/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.urls import path
+
+
+urlpatterns = [
+    path("public/", lambda request: HttpResponse(), name="public"),
+    path("private/", login_required(lambda request: HttpResponse()), name="private"),
+]
