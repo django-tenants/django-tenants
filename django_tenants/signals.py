db57c5845ab2bd71690faff0cfc4efe7e1ce9d14
Tom Turner <tom@twt.me.uk>
2020-08-24 09:35:47 +0100

    RemovedInDjango40Warning

--- django_tenants/signals.py
+++ django_tenants/signals.py
@@ -2,24 +2,35 @@ from django.db.models.signals import post_delete
 from django.dispatch import Signal, receiver
 from django_tenants.utils import get_tenant_model, schema_exists
 
-post_schema_sync = Signal(providing_args=['tenant'])
+post_schema_sync = Signal()
 post_schema_sync.__doc__ = """
+
 Sent after a tenant has been saved, its schema created and synced
+
+Argument Required = tenant
+
 """
 
-schema_needs_to_be_sync = Signal(providing_args=['tenant'])
+schema_needs_to_be_sync = Signal()
 schema_needs_to_be_sync.__doc__ = """
 Schema needs to be synced
+
+Argument Required = tenant
+
 """
 
-schema_migrated = Signal(providing_args=['schema_name'])
+schema_migrated = Signal()
 schema_migrated.__doc__ = """
 Sent after migration has finished on a schema
+
+Argument Required = schema_name
 """
 
-schema_migrate_message = Signal(providing_args=['message'])
+schema_migrate_message = Signal()
 schema_migrate_message.__doc__ = """
 Sent when a message is generated in run migration
+
+Argument Required = message
 """
 
 
