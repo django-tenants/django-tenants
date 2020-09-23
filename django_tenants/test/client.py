4a271348cbe2c7d25bff3193c1fcc5360cd8ce4f
Kryštof Pilnáček <krystof.pilnacek@rossum.ai>
2020-09-23 13:30:06 +0200

    Reuse .generic method in TenantRequestFactory

--- django_tenants/test/client.py
+++ django_tenants/test/client.py
@@ -11,49 +11,13 @@ class TenantRequestFactory(RequestFactory):
         super().__init__(**defaults)
         self.tenant = tenant
 
-    def get(self, path, data={}, **extra):
-        if 'HTTP_HOST' not in extra:
-            extra['HTTP_HOST'] = self.tenant.get_primary_domain().domain
-
-        return super().get(path, data, **extra)
-
-    def post(self, path, data={}, **extra):
-        if 'HTTP_HOST' not in extra:
-            extra['HTTP_HOST'] = self.tenant.get_primary_domain().domain
-
-        return super().post(path, data, **extra)
-
-    def patch(self, path, data={}, **extra):
-        if 'HTTP_HOST' not in extra:
-            extra['HTTP_HOST'] = self.tenant.get_primary_domain().domain
-
-        return super().patch(path, data, **extra)
-
-    def put(self, path, data={}, **extra):
-        if 'HTTP_HOST' not in extra:
-            extra['HTTP_HOST'] = self.tenant.get_primary_domain().domain
-
-        return super().put(path, data, **extra)
-
-    def delete(self, path, data='', **extra):
-        if 'HTTP_HOST' not in extra:
-            extra['HTTP_HOST'] = self.tenant.get_primary_domain().domain
-
-        return super().delete(path, data, **extra)
-
-
-class TenantClient(Client):
-    tm = TenantMainMiddleware()
-
-    def __init__(self, tenant, enforce_csrf_checks=False, **defaults):
-        super().__init__(enforce_csrf_checks, **defaults)
-        self.tenant = tenant
-
     def generic(self, *args, **kwargs):
         if "HTTP_HOST" not in kwargs:
             kwargs["HTTP_HOST"] = self.tenant.get_primary_domain().domain
         return super().generic(*args, **kwargs)
 
+
+class TenantClient(TenantRequestFactory, Client):
     def login(self, **credentials):
         # Create a dummy HttpRequest object and add HTTP_HOST
 
