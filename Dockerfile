4b70af9abfdd9e41cd0a4b775b5a77547185108f
Tom Turner <tom@twt.me.uk>
2020-11-25 16:49:07 +0000

    Restored the way of runing the test in docker
    docker-compose run django-tenants-test

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ RUN mkdir /code
 VOLUME /code
 WORKDIR /code
 COPY requirements.txt /code/
+RUN apt-get update && apt-get -y install postgresql libpq-dev postgresql-client postgresql-client-common python3-pip git netcat
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . /code/
--- dts_test_project/dts_test_project/settings.py
+++ dts_test_project/dts_test_project/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'USER': os.environ.get('DATABASE_USER', 'postgres'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'root'),
         'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
-        'PORT': '',
+        'PORT': os.environ.get('DATABASE_PORT', 5432),
     }
 }
 
