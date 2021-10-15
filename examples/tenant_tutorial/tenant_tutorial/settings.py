658186b8f2f4643e1bcc8a55872b8fd5866edfff
tomturner <tom@twt.me.uk>
2021-10-15 21:40:49 +0100

    set USE_TZ to False. see #669

--- examples/tenant_tutorial/tenant_tutorial/settings.py
+++ examples/tenant_tutorial/tenant_tutorial/settings.py
@@ -54,7 +54,7 @@ USE_I18N = True
 USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
+USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
