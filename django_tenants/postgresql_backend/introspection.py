c5370993566f789f3fc012a3afe4c034041f430a
Christopher Thorne <libcthorne@users.noreply.github.com>
2022-09-12 15:50:06 +0100

    Fix quotation marks being included in search path
    Example where search path is quoted:
    ```
    db=# SET search_path = 'country_BD';
    SET
    farm2go=# SHOW search_path;
     search_path
    --------------
     "country_BD"
    (1 row)
    ```

--- django_tenants/postgresql_backend/introspection.py
+++ django_tenants/postgresql_backend/introspection.py
@@ -13,12 +13,15 @@ class DatabaseSchemaIntrospectionSearchPathContext:
 
     def __enter__(self):
         self.cursor.execute('SHOW search_path')
-        self.original_search_path = self.cursor.fetchone()[0].split(',')
+        self.original_search_path = [
+            search_path.strip().replace('"', '')
+            for search_path in self.cursor.fetchone()[0].split(',')
+        ]
         self.cursor.execute(f"SET search_path = '{self.connection.schema_name}'")
 
     def __exit__(self, *args, **kwargs):
         formatted_search_paths = ', '.join(
-            f"'{search_path.strip()}'"
+            f"'{search_path}'"
             for search_path in self.original_search_path
         )
         self.cursor.execute(f'SET search_path = {formatted_search_paths}')
