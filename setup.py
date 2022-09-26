0cbab0d5c86df33951e5df127e33fc286b329c03
Bruno Alla <alla.brunoo@gmail.com>
2022-09-26 13:58:40 +0100

    Add missing entries in Trove classifiers
    Django 3.2 and 4.1 are supported
    Python 3.10 is supported

--- setup.py
+++ setup.py
@@ -45,13 +45,16 @@ setup(
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
         'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
         'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.1',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
     install_requires=[
