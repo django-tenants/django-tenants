840986328a02b744cf649dab6a2ff06d89069956
Tom Turner <tom@twt.me.uk>
2023-12-04 21:10:36 +0000

    Updated the version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-__version__ = "3.5.0"
+__version__ = "3.6.0"
 
 setup(
     name='django-tenants',
@@ -41,11 +41,6 @@ setup(
         'Environment :: Web Environment',
         'License :: OSI Approved :: MIT License',
         'Framework :: Django',
-        'Framework :: Django :: 2.1',
-        'Framework :: Django :: 2.2',
-        'Framework :: Django :: 3.0',
-        'Framework :: Django :: 3.1',
-        'Framework :: Django :: 3.2',
         'Framework :: Django :: 4.0',
         'Framework :: Django :: 4.1',
         'Framework :: Django :: 4.2',
