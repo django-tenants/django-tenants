2860690b539e3f51ce7cb550ba40ac23db41332e
jmcdonnell <jmcdonnell@fieldbox.ai>
2024-07-16 11:04:42 +0200

    fix: adapt urlresolvers tests to use URLConf object

--- django_tenants/urlresolvers.py
+++ django_tenants/urlresolvers.py
@@ -1,15 +1,13 @@
-import re
-import sys
 from types import ModuleType
 
-from django.db import connection
 from django.conf import settings
-from django.urls import URLResolver, reverse as reverse_default, path, include
+from django.urls import reverse as reverse_default, path, include
 from django.utils.functional import lazy
 from django_tenants.utils import (
-    get_tenant_domain_model,
     get_subfolder_prefix,
-    clean_tenant_url, has_multi_type_tenants, get_tenant_types,
+    clean_tenant_url,
+    has_multi_type_tenants,
+    get_tenant_types,
 )
 
 
@@ -22,15 +20,19 @@ reverse_lazy = lazy(reverse, str)
 
 
 def get_subfolder_urlconf(tenant):
-   if has_multi_type_tenants():
-       urlconf = get_tenant_types()[tenant.get_tenant_type()]['URLCONF']
-   else:
-       urlconf = settings.ROOT_URLCONF
+    if has_multi_type_tenants():
+        urlconf = get_tenant_types()[tenant.get_tenant_type()]["URLCONF"]
+    else:
+        urlconf = settings.ROOT_URLCONF
 
-   subfolder_prefix = get_subfolder_prefix()
-   class TenantUrlConf(ModuleType):
-       urlpatterns = [
-           path(f"{subfolder_prefix}/{tenant.domain_subfolder}/", include(urlconf))
-       ]
+    subfolder_prefix = get_subfolder_prefix()
 
-   return TenantUrlConf(tenant.domain_subfolder)
+    class TenantUrlConf(ModuleType):
+        urlpatterns = [
+            path(
+                f"{subfolder_prefix}/{tenant.domain_subfolder}/",
+                include(urlconf),
+            )
+        ]
+
+    return TenantUrlConf(tenant.domain_subfolder)
