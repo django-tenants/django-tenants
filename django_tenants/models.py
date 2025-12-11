34ea6278adb80954709a7dde1177a03034ff8438
Karol Onyśko <karol.onysko@cic.com>
2025-12-11 11:55:39 +0100

    Added initial support for mysql

--- django_tenants/models.py
+++ django_tenants/models.py
@@ -152,7 +152,7 @@ class TenantMixin(models.Model):
         if has_schema and schema_exists(self.schema_name) and (self.auto_drop_schema or force_drop):
             self.pre_drop()
             cursor = connection.cursor()
-            cursor.execute('DROP SCHEMA "%s" CASCADE' % self.schema_name)
+            cursor.execute('DROP DATABASE %s CASCADE' % self.schema_name)
 
     def pre_drop(self):
         """
---
+++ django_tenants/mysql_backend/introspection.py
@@ -0,0 +1,50 @@
+from django.db.backends.mysql.introspection import DatabaseIntrospection
+
+
+class DatabaseSchemaIntrospectionSearchPathContext:
+    """
+    This context manager restores the original search path of the cursor
+    once the method of the introspection class has been called.
+    """
+    def __init__(self, cursor, connection):
+        self.cursor = cursor
+        self.connection = connection
+        self.original_search_path = None
+
+    def __enter__(self):
+        self.cursor.execute('SELECT DATABASE()')
+        self.original_search_path = self.cursor.fetchone()[0]
+        self.cursor.execute('USE %s', [self.connection.schema_name])
+
+    def __exit__(self, *args, **kwargs):
+        self.cursor.execute('USE %s', [self.original_search_path])
+
+
+class DatabaseSchemaIntrospection(DatabaseIntrospection):
+    """
+    database schema introspection class
+    """
+
+    def get_table_list(self, cursor):
+        with DatabaseSchemaIntrospectionSearchPathContext(cursor=cursor, connection=self.connection):
+            return super().get_table_list(cursor)
+
+    def get_table_description(self, cursor, table_name):
+        with DatabaseSchemaIntrospectionSearchPathContext(cursor=cursor, connection=self.connection):
+            return super().get_table_description(cursor, table_name)
+
+    def get_sequences(self, cursor, table_name, table_fields=()):
+        with DatabaseSchemaIntrospectionSearchPathContext(cursor=cursor, connection=self.connection):
+            return super().get_sequences(cursor, table_name, table_fields)
+
+    def get_key_columns(self, cursor, table_name):
+        with DatabaseSchemaIntrospectionSearchPathContext(cursor=cursor, connection=self.connection):
+            return super().get_key_columns(cursor, table_name)
+
+    def get_constraints(self, cursor, table_name):
+        with DatabaseSchemaIntrospectionSearchPathContext(cursor=cursor, connection=self.connection):
+            return super().get_constraints(cursor, table_name)
+
+    def get_storage_engine(self, cursor, table_name):
+        with DatabaseSchemaIntrospectionSearchPathContext(cursor=cursor, connection=self.connection):
+            return super().get_storage_engine(cursor, table_name)
