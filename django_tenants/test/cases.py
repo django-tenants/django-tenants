a05b138cc6946f750fad2e663a3d1f664ca67e44
Karol Onyśko <karol.onysko@cic.com>
2026-01-05 09:41:07 +0100

    Added transactiontestcase

--- django_tenants/test/cases.py
+++ django_tenants/test/cases.py
@@ -1,11 +1,93 @@
 from django.core.management import call_command
 from django.conf import settings
 from django.db import connection, connections
-from django.test import TestCase
+from django.test import TestCase, TransactionTestCase
 from django_tenants.utils import get_tenant_model, get_tenant_domain_model, get_public_schema_name
 
 
 class TenantTestCase(TestCase):
+    tenant = None
+    domain = None
+
+    @classmethod
+    def setup_tenant(cls, tenant):
+        """
+        Add any additional setting to the tenant before it get saved. This is required if you have
+        required fields.
+        :param tenant:
+        :return:
+        """
+        pass
+
+    @classmethod
+    def setup_domain(cls, domain):
+        """
+        Add any additional setting to the domain before it get saved. This is required if you have
+        required fields.
+        :param domain:
+        :return:
+        """
+        pass
+
+    @classmethod
+    def setUpClass(cls):
+        cls.sync_shared()
+        cls.add_allowed_test_domain()
+        cls.tenant = get_tenant_model()(schema_name=cls.get_test_schema_name())
+        cls.setup_tenant(cls.tenant)
+        cls.tenant.save(verbosity=cls.get_verbosity())
+
+        # Set up domain
+        tenant_domain = cls.get_test_tenant_domain()
+        cls.domain = get_tenant_domain_model()(tenant=cls.tenant, domain=tenant_domain)
+        cls.setup_domain(cls.domain)
+        cls.domain.save()
+
+        connection.set_tenant(cls.tenant)
+
+    @classmethod
+    def tearDownClass(cls):
+        connection.set_schema_to_public()
+        cls.domain.delete()
+        cls.tenant.delete(force_drop=True)
+        cls.remove_allowed_test_domain()
+
+    @classmethod
+    def get_verbosity(cls):
+        return 0
+
+    @classmethod
+    def add_allowed_test_domain(cls):
+        tenant_domain = cls.get_test_tenant_domain()
+
+        # ALLOWED_HOSTS is a special setting of Django setup_test_environment so we can't modify it with helpers
+        if tenant_domain not in settings.ALLOWED_HOSTS:
+            settings.ALLOWED_HOSTS += [tenant_domain]
+
+    @classmethod
+    def remove_allowed_test_domain(cls):
+        tenant_domain = cls.get_test_tenant_domain()
+
+        if tenant_domain in settings.ALLOWED_HOSTS:
+            settings.ALLOWED_HOSTS.remove(tenant_domain)
+
+    @classmethod
+    def sync_shared(cls):
+        call_command('migrate_schemas',
+                     schema_name=get_public_schema_name(),
+                     interactive=False,
+                     verbosity=0)
+
+    @classmethod
+    def get_test_tenant_domain(cls):
+        return 'tenant.test.com'
+
+    @classmethod
+    def get_test_schema_name(cls):
+        return 'test'
+
+
+class TransactionTenantTestCase(TransactionTestCase):
     tenant = None
     domain = None
 
@@ -121,7 +203,7 @@ class FastTenantTestCase(TenantTestCase):
 
     @classmethod
     def get_test_schema_name(cls):
-        return 'fast_test'
+        return 'test_fast'
 
     @classmethod
     def setup_test_tenant_and_domain(cls):
@@ -164,7 +246,6 @@ class FastTenantTestCase(TenantTestCase):
         cls._rollback_atomics(cls.cls_atomics)
         for conn in connections.all(initialized_only=True):
             conn.close()
-
 
     def _fixture_teardown(self):
         if self.flush_data():
